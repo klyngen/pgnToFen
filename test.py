86bee59e34a82012429cb4b1e37a4f8f0991b01f
Martin Klingenberg <martin.klingenberg@hig.no>
2017-05-08 04:10:21 +0200

    shows how to choose every third line. and write to file

--- test.py
+++ test.py
@@ -7,8 +7,15 @@ stats = converter.pgnFile(file)
 
 output = open("fenstring.txt", "w")
 
-for fen in (stats['succeeded'][0][1]):
-    output.write(fen)
+#for fen in (stats['succeeded'][0][1]):
+#    output.write(fen)
+#    output.write('\n')
+
+
+items = stats['succeeded'][0][1]
+
+for x in range(0, len(items), 3):
+    output.write(items[x]);
     output.write('\n')
 
 output.close()
